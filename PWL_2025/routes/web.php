071463d48b6f057455df641ac40abae954440b60
arimbiph4 <andikaputra022005@gmail.com>
2025-02-20 21:18:07 +0700

    Tugas pertama

--- PWL_2025/routes/web.php
+++ PWL_2025/routes/web.php
@@ -1,6 +1,9 @@
-<?php
+<?php     //menandakan awal dari file PHP
 
+
+//mengimpor Route untuk mendefinisikan rute, data ItemController agar bisa digunakan dalam routing
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//komentar yang menjelaskan bahwa file digunakan untuk mendefinisikan rute web dalam aplikasi laravel
+
+Route::get('/', function () {   //mendefinisikan rute untuk URL utama(/)
+    return view('welcome');    //saat diakses akan menampilkan halaman welcome
 });
+
+Route::resource('items', ItemController::class);   //membuat rute otomatis untuk items dengan menggunakan ItemController
